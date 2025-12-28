2768fd2daee50f2fc01dec7eb0d11b208a00c6dc
KomalDeevaoy <komaldeevaroy@gmail.com>
2025-12-28 15:24:14 +0530

    Logo added

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,6 +10,7 @@ import {
 import AuthModal from "./Authentication/AuthModal";
 import { CryptoState } from "../CryptoContext";
 import UserSidebar from "./Authentication/UserSidebar";
+import logo from "../assets/logo.png"; 
 
 const Header = () => {
   const { currency, setCurrency, user } = CryptoState();
@@ -18,33 +19,47 @@ const Header = () => {
     <AppBar position="static" color="transparent">
       <Container>
         <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
-          
+
           {/* LEFT SECTION */}
-          <Typography
-            variant="h6"
-            sx={{ fontWeight: "bold", cursor: "pointer" }}
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              gap: 1.5,
+              cursor: "pointer",
+            }}
           >
-            Crypto Hunter
-          </Typography>
+            <img
+              src={logo}
+              alt="Crypto Hunter Logo"
+              style={{
+                height: "35px",   // 🔑 keeps header stable
+                width: "auto",
+              }}
+            />
+
+            <Typography
+              variant="h6"
+              sx={{ fontWeight: "bold" }}
+            >
+              Crypto Hunter
+            </Typography>
+          </Box>
 
           {/* RIGHT SECTION */}
           <Box sx={{ display: "flex", alignItems: "center", gap: 3 }}>
-            
-            {/* Currency Selector */}
             <Select
               value={currency}
               onChange={(e) => setCurrency(e.target.value)}
               sx={{
                 height: 40,
                 color: "white",
-                borderColor: "white",
               }}
             >
               <MenuItem value="INR">INR</MenuItem>
               <MenuItem value="USD">USD</MenuItem>
             </Select>
 
-            {/* Login / User */}
             {user ? <UserSidebar /> : <AuthModal />}
           </Box>
 
